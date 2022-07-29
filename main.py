b1397867611cde340f92007a9507a601abe9a49d
Guga Katamadze <gkatam@cryptx.com>
2022-07-29 13:34:09 +0400

    argparse initial setup

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+import argparse
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+parser = argparse.ArgumentParser(description='Hack Simulator')
+parser.add_argument('path', type=str, metavar='path', help="absolute or relative path to the hack file")
 
+args = parser.parse_args()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(args.path)
